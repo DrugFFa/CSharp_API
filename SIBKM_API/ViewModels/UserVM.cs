07f22e51eec12291d52490ac98d1ff4147868e20
DrugFFa <articuno712@gmail.com>
2022-10-21 11:17:57 +0700

    Initial Commit

---
+++ SIBKM_API/ViewModels/UserVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIBKM_API.ViewModels
+{
+    public class UserVM
+    {
+        public int Id { get; set; }
+        public string Password { get;  set; }
+        
+    }
+}
